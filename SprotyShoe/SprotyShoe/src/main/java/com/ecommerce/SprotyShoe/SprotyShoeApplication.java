07b7b7eabdf7fbdf1d618a64306614cf0c266545
“Lokesh <glokeshganapathy@gmail.com>
2022-02-10 11:56:34 +0530

    Inital Spring Boot project

---
+++ SprotyShoe/SprotyShoe/src/main/java/com/ecommerce/SprotyShoe/SprotyShoeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.SprotyShoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprotyShoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprotyShoeApplication.class, args);
+	}
+
+}
---
+++ SprotyShoe/SprotyShoe/src/test/java/com/ecommerce/SprotyShoe/SprotyShoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.SprotyShoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprotyShoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
